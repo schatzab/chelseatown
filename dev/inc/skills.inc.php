fad213ad87b1a80be655c9c187584b32dcc96388
Chelsea <runinbend@gmail.com>
2015-11-20 13:35:56 -0800

    DRY skills page - JS updates

--- dev/inc/skills.inc.php
+++ dev/inc/skills.inc.php
@@ -4,18 +4,16 @@
     <header class="page-header">
       <h3>Some skills I'm proud of</h3>      
     </header>
-    <div id="skills_loop"></div>
+    <div id="skills_loop" onLoad="loadSkills();"></div>
   </div>
 </section>
 
 <script>
-document.getElementById("skills_loop").addEventListener("load", loadSkills);  // load function
-var skills = ["html5", "css3", "js", "bootstrap"]; // skills array
-
+var skills = ["html5", "css3", "js", "bootstrap"]; // ** Add more boxes to the array here!
 function loadSkills() {
   for (var i = 0; i < skills.length; i++) {
-      document.getElementById("skills_loop").innerHTML = "<div class='col-xs-6 col-md-3'><a href='#' class='thumbnail'><img src='images/skills_icon_" + skills[i] + ".png' alt='" + skills[i] + "'></a></div>";
-      console.log(skills[i]);
+      document.getElementById("skills_loop").innerHTML += "<div class='col-xs-6 col-md-3'><a href='JavaScript:void(0)' class='thumbnail'><img src='images/skills_icon_" + skills[i] + ".png' alt='" + skills[i] + "'></a></div>";
   }
 }
+window.onload = loadSkills;
 </script>
