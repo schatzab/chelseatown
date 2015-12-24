7653e442cb7532bf39b657f331230a764d567d32
Chelsea <runinbend@gmail.com>
2015-12-24 07:17:36 -0800

    some style changes

--- dev/err/401.php
+++
@@ -1,4 +0,0 @@
-<section class="jumbotron">
-	<h2> Opps, you've reached this page in error</h2>
-	<a href="/">Go Home</a>
-</section>
--- dev/inc/footer.inc.php
+++
@@ -1,6 +0,0 @@
-
-<footer class="container-fluid text-center">
-  	<!-- Scroll Top -->
-	<a href="javascript:" id="scroll_top"><b class="scroll_top_arrow"><span class="glyphicon glyphicon-chevron-up"></span></b></a>
-  <p>Coded & Designed by Chelsea Town &copy; <?php echo date("Y"); ?></p>		
-</footer>
