337842fc777a10427e02d76ce1e1c45ad083eacc
Chelsea <me@chelseatown.com>
2017-11-05 20:36:17 -0800

    one more copy change on about

--- inc/about.inc.php
+++ inc/about.inc.php
@@ -8,10 +8,10 @@
 		<div class="about_box_right">
 			<h1>Good day, <br>I'm Chelsea</h1>
 			<div class="about_main_box_text">
-				I was raised on the island of O'ahu, then educated at UCSB and the school of life. Bend, Oregon is now my home with my adventure-loving family of four. I believe in learning as a life-long journey and try to absorb as much as I can.  My goals are to work with a company which enjoys change and working towards progressive development.  
+				Raised on the island of O'ahu, I grew up an island girl until I moved to California where I received my Bachelor's degree at UCSB. I continued, educating myself with experience and trial & error. Bend, Oregon is now my home with my adventure-loving family of four. Hiking, skateboarding, skiing, running and enjoying this amazing state are some of the things we do whenever time allows. I believe in learning as a lifelong journey and try to absorb as much as I can through books and what's shared on the Internet.  
 
 				<br><br>
-				My skills are technology-based and I'm a quick learner with many interests. If you'd like specifics, I'm glad to chat more about them!
+				My goals are to work with a company which enjoys change, strives for common goals, treats each other with (and expects) respect, and aims for progressive development. My skills are technology-based and I'm a quick learner with many interests. If you'd like to get into specifics, I'm glad to chat more about them!
 			</div>
 		</div>
 </div>
